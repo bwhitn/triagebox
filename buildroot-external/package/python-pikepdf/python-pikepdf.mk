845bbd022a3cc235b089a4fdcefdae42dc13fe0d
bwhitn <brian.m.whitney@outlook.com>
2026-02-23 11:24:45 -0500

    fixing pikepdf

--- buildroot-external/package/python-pikepdf/python-pikepdf.mk
+++ buildroot-external/package/python-pikepdf/python-pikepdf.mk
@@ -9,6 +9,22 @@ PYTHON_PIKEPDF_SOURCE = pikepdf-9.4.2.tar.gz
 PYTHON_PIKEPDF_SITE = https://files.pythonhosted.org/packages/57/54/71552c3ab2694618741e5579506759238716664400304cb615917a993df5
 PYTHON_PIKEPDF_SETUP_TYPE = setuptools
 PYTHON_PIKEPDF_LICENSE = UNKNOWN
-PYTHON_PIKEPDF_DEPENDENCIES = python-pybind
+PYTHON_PIKEPDF_DEPENDENCIES = host-python-pip
+PYTHON_PIKEPDF_PYBIND11_VERSION = 2.13.6
+
+define PYTHON_PIKEPDF_INSTALL_PYBIND11
+	rm -rf $(@D)/.pybind11-wheelhouse
+	mkdir -p $(@D)/.pybind11-wheelhouse
+	PIP_DISABLE_PIP_VERSION_CHECK=1 \
+	PIP_NO_CACHE_DIR=1 \
+	python3 -m pip download --only-binary=:all: --dest $(@D)/.pybind11-wheelhouse \
+		pybind11==$(PYTHON_PIKEPDF_PYBIND11_VERSION)
+	PIP_DISABLE_PIP_VERSION_CHECK=1 \
+	PIP_NO_CACHE_DIR=1 \
+	$(HOST_DIR)/bin/python3 -m pip install --no-index --find-links $(@D)/.pybind11-wheelhouse \
+		pybind11==$(PYTHON_PIKEPDF_PYBIND11_VERSION)
+	rm -rf $(@D)/.pybind11-wheelhouse
+endef
+PYTHON_PIKEPDF_PRE_BUILD_HOOKS += PYTHON_PIKEPDF_INSTALL_PYBIND11
 
 $(eval $(python-package))
