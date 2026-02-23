41c394874999000c54267ba3ad876ba244d20cc2
bwhitn <brian.m.whitney@outlook.com>
2026-02-23 10:25:13 -0500

    fixing icicle

--- buildroot-external/package/python-icicle-emu/python-icicle-emu.mk
+++ buildroot-external/package/python-icicle-emu/python-icicle-emu.mk
@@ -7,7 +7,8 @@
 PYTHON_ICICLE_EMU_VERSION = 0.0.11
 PYTHON_ICICLE_EMU_SOURCE = icicle_emu-0.0.11.tar.gz
 PYTHON_ICICLE_EMU_SITE = https://files.pythonhosted.org/packages/4f/b0/a2efc55e93b05e9f734a93ce0287992695bdea1510c4ee0297bf48f1bad8
-PYTHON_ICICLE_EMU_SETUP_TYPE = pep517
+# Use Buildroot's maturin setup path so cargo uses target cross settings.
+PYTHON_ICICLE_EMU_SETUP_TYPE = maturin
 PYTHON_ICICLE_EMU_LICENSE = UNKNOWN
 
 $(eval $(python-package))
