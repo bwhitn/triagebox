b8a235abf7c853d427278b56b1b02cf00280e55c
bwhitn <brian.m.whitney@outlook.com>
2026-02-23 11:45:57 -0500

    fixing py7zr

--- buildroot-external/package/python-py7zr/python-py7zr.mk
+++ buildroot-external/package/python-py7zr/python-py7zr.mk
@@ -10,4 +10,10 @@ PYTHON_PY7ZR_SITE = https://files.pythonhosted.org/packages/a1/99/0acbe1ef4c4f1a
 PYTHON_PY7ZR_SETUP_TYPE = setuptools
 PYTHON_PY7ZR_LICENSE = UNKNOWN
 
+define PYTHON_PY7ZR_FIX_PYPROJECT_LICENSE
+	$(SED) 's|^license = "LGPL-2.1-or-later"$$|license = { text = "LGPL-2.1-or-later" }|g' \
+		$(@D)/pyproject.toml
+endef
+PYTHON_PY7ZR_POST_PATCH_HOOKS += PYTHON_PY7ZR_FIX_PYPROJECT_LICENSE
+
 $(eval $(python-package))
