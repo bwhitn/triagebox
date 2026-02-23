e26481d54da8ecbaad60a99d6ef2761944eb2f67
bwhitn <brian.m.whitney@outlook.com>
2026-02-23 12:11:30 -0500

    fixing py7zr

--- buildroot-external/package/python-py7zr/python-py7zr.mk
+++ buildroot-external/package/python-py7zr/python-py7zr.mk
@@ -9,7 +9,7 @@ PYTHON_PY7ZR_SOURCE = py7zr-1.1.0rc4.tar.gz
 PYTHON_PY7ZR_SITE = https://files.pythonhosted.org/packages/a1/99/0acbe1ef4c4f1a09fd5f96f2c24f77179c2b2e566686d2ac80646a4603ff
 PYTHON_PY7ZR_SETUP_TYPE = setuptools
 PYTHON_PY7ZR_LICENSE = UNKNOWN
-PYTHON_PY7ZR_DEPENDENCIES = host-python-setuptools-scm
+PYTHON_PY7ZR_DEPENDENCIES = host-python-setuptools-scm host-python-pip
 
 define PYTHON_PY7ZR_FIX_PYPROJECT_LICENSE
 	$(SED) 's|^license = "LGPL-2.1-or-later"$$|license = { text = "LGPL-2.1-or-later" }|g' \
@@ -18,9 +18,32 @@ endef
 PYTHON_PY7ZR_POST_PATCH_HOOKS += PYTHON_PY7ZR_FIX_PYPROJECT_LICENSE
 
 define PYTHON_PY7ZR_RELAX_BUILD_BACKEND_REQS
-	$(SED) 's|setuptools>=80|setuptools|g' $(@D)/pyproject.toml
-	$(SED) 's|setuptools_scm\\[toml\\]>=9.2.0|setuptools_scm[toml]|g' $(@D)/pyproject.toml
+	if [ -f $(@D)/pyproject.toml ]; then \
+		$(SED) 's|setuptools>=80|setuptools|g' $(@D)/pyproject.toml; \
+		$(SED) 's|setuptools_scm\\[toml\\]>=9.2.0|setuptools_scm[toml]|g' $(@D)/pyproject.toml; \
+	fi
+	if [ -f $(@D)/setup.py ]; then \
+		$(SED) 's|setuptools_scm\\[toml\\]>=9.2.0|setuptools_scm[toml]|g' $(@D)/setup.py; \
+	fi
+	if [ -f $(@D)/setup.cfg ]; then \
+		$(SED) 's|setuptools_scm\\[toml\\]>=9.2.0|setuptools_scm[toml]|g' $(@D)/setup.cfg; \
+	fi
 endef
 PYTHON_PY7ZR_POST_PATCH_HOOKS += PYTHON_PY7ZR_RELAX_BUILD_BACKEND_REQS
+
+define PYTHON_PY7ZR_INSTALL_BUILD_BACKEND_DEPS
+	rm -rf $(@D)/.py7zr-wheelhouse
+	mkdir -p $(@D)/.py7zr-wheelhouse
+	PIP_DISABLE_PIP_VERSION_CHECK=1 \
+	PIP_NO_CACHE_DIR=1 \
+	python3 -m pip download --only-binary=:all: --dest $(@D)/.py7zr-wheelhouse \
+		"setuptools>=80" "setuptools_scm[toml]>=9.2.0"
+	PIP_DISABLE_PIP_VERSION_CHECK=1 \
+	PIP_NO_CACHE_DIR=1 \
+	$(HOST_DIR)/bin/python3 -m pip install --no-index --find-links $(@D)/.py7zr-wheelhouse \
+		"setuptools>=80" "setuptools_scm[toml]>=9.2.0"
+	rm -rf $(@D)/.py7zr-wheelhouse
+endef
+PYTHON_PY7ZR_PRE_BUILD_HOOKS += PYTHON_PY7ZR_INSTALL_BUILD_BACKEND_DEPS
 
 $(eval $(python-package))
