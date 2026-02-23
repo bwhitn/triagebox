7b91a04a0ba16a352bdd191b830dfca45ac16a67
bwhitn <brian.m.whitney@outlook.com>
2026-02-23 12:09:07 -0500

    fixing py7zr

--- buildroot-external/package/python-py7zr/python-py7zr.mk
+++ buildroot-external/package/python-py7zr/python-py7zr.mk
@@ -9,11 +9,18 @@ PYTHON_PY7ZR_SOURCE = py7zr-1.1.0rc4.tar.gz
 PYTHON_PY7ZR_SITE = https://files.pythonhosted.org/packages/a1/99/0acbe1ef4c4f1a09fd5f96f2c24f77179c2b2e566686d2ac80646a4603ff
 PYTHON_PY7ZR_SETUP_TYPE = setuptools
 PYTHON_PY7ZR_LICENSE = UNKNOWN
+PYTHON_PY7ZR_DEPENDENCIES = host-python-setuptools-scm
 
 define PYTHON_PY7ZR_FIX_PYPROJECT_LICENSE
 	$(SED) 's|^license = "LGPL-2.1-or-later"$$|license = { text = "LGPL-2.1-or-later" }|g' \
 		$(@D)/pyproject.toml
 endef
 PYTHON_PY7ZR_POST_PATCH_HOOKS += PYTHON_PY7ZR_FIX_PYPROJECT_LICENSE
+
+define PYTHON_PY7ZR_RELAX_BUILD_BACKEND_REQS
+	$(SED) 's|setuptools>=80|setuptools|g' $(@D)/pyproject.toml
+	$(SED) 's|setuptools_scm\\[toml\\]>=9.2.0|setuptools_scm[toml]|g' $(@D)/pyproject.toml
+endef
+PYTHON_PY7ZR_POST_PATCH_HOOKS += PYTHON_PY7ZR_RELAX_BUILD_BACKEND_REQS
 
 $(eval $(python-package))
