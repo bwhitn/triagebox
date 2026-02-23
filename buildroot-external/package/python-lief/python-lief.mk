e7a5ad6e625e52564816f72db5e137c7117a0aa8
bwhitn <brian.m.whitney@outlook.com>
2026-02-23 10:43:39 -0500

    fixing lief

--- buildroot-external/package/python-lief/python-lief.mk
+++ buildroot-external/package/python-lief/python-lief.mk
@@ -5,9 +5,18 @@
 ################################################################################
 
 PYTHON_LIEF_VERSION = 0.17.3
-PYTHON_LIEF_SOURCE = lief-0.17.3.tar.gz
-PYTHON_LIEF_SITE = https://files.pythonhosted.org/packages/ea/7d/ba1b4e896712f5c161a6ef6eb546aaaecacf10f7c0ae8890b74359c62655
+PYTHON_LIEF_SITE = $(call github,lief-project,LIEF,$(PYTHON_LIEF_VERSION))
+PYTHON_LIEF_SUBDIR = api/python
 PYTHON_LIEF_SETUP_TYPE = pep517
 PYTHON_LIEF_LICENSE = Apache License 2.0
+PYTHON_LIEF_DEPENDENCIES = host-cmake host-ninja host-python-pip
+
+define PYTHON_LIEF_INSTALL_BUILD_REQUIREMENTS
+	PIP_DISABLE_PIP_VERSION_CHECK=1 \
+	PIP_NO_CACHE_DIR=1 \
+	$(HOST_DIR)/bin/python3 -m pip install --no-cache-dir \
+		-r $(@D)/api/python/build-requirements.txt
+endef
+PYTHON_LIEF_PRE_BUILD_HOOKS += PYTHON_LIEF_INSTALL_BUILD_REQUIREMENTS
 
 $(eval $(python-package))
