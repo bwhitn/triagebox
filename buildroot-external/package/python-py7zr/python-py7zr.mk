adb1a818d72383609bd48885936849e77b5155a4
bwhitn <brian.m.whitney@outlook.com>
2026-02-24 00:26:14 -0500

    fixing lzma not using backports but python lzma

--- buildroot-external/package/python-py7zr/python-py7zr.mk
+++ buildroot-external/package/python-py7zr/python-py7zr.mk
@@ -10,7 +10,7 @@ PYTHON_PY7ZR_SITE = https://files.pythonhosted.org/packages/a1/99/0acbe1ef4c4f1a
 PYTHON_PY7ZR_SETUP_TYPE = setuptools
 PYTHON_PY7ZR_LICENSE = UNKNOWN
 PYTHON_PY7ZR_DEPENDENCIES = host-python-setuptools-scm host-python-pip \
-	python-backports-lzma python-inflate64 python-multivolumefile python-pybcj
+	python-inflate64 python-multivolumefile python-pybcj
 
 define PYTHON_PY7ZR_FIX_PYPROJECT_LICENSE
 	$(SED) 's|^license = "LGPL-2.1-or-later"$$|license = { text = "LGPL-2.1-or-later" }|g' \
@@ -31,6 +31,14 @@ define PYTHON_PY7ZR_RELAX_BUILD_BACKEND_REQS
 	fi
 endef
 PYTHON_PY7ZR_POST_PATCH_HOOKS += PYTHON_PY7ZR_RELAX_BUILD_BACKEND_REQS
+
+define PYTHON_PY7ZR_FORCE_STDLIB_LZMA
+	if [ -f $(@D)/py7zr/compressor.py ]; then \
+		$(SED) 's|from backports import lzma|import lzma|g' $(@D)/py7zr/compressor.py; \
+		$(SED) 's|import backports\\.lzma as lzma|import lzma|g' $(@D)/py7zr/compressor.py; \
+	fi
+endef
+PYTHON_PY7ZR_POST_PATCH_HOOKS += PYTHON_PY7ZR_FORCE_STDLIB_LZMA
 
 define PYTHON_PY7ZR_INSTALL_BUILD_BACKEND_DEPS
 	rm -rf $(@D)/.py7zr-wheelhouse
