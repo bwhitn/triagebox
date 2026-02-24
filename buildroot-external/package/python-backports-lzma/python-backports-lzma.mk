830fffe1cc97aea5b1b55108c72563ade3f690ce
bwhitn <brian.m.whitney@outlook.com>
2026-02-24 00:19:05 -0500

    fixing lzma backports

--- buildroot-external/package/python-backports-lzma/python-backports-lzma.mk
+++ buildroot-external/package/python-backports-lzma/python-backports-lzma.mk
@@ -11,6 +11,25 @@ PYTHON_BACKPORTS_LZMA_SETUP_TYPE = setuptools
 PYTHON_BACKPORTS_LZMA_LICENSE = Python-2.0
 PYTHON_BACKPORTS_LZMA_DEPENDENCIES = xz
 
+define PYTHON_BACKPORTS_LZMA_SANITIZE_SETUP
+	if [ -f $(@D)/setup.py ]; then \
+		for path in /usr/local/include /opt/local/include /usr/local/lib /opt/local/lib; do \
+			$(SED) "s|'$$path', *||g" $(@D)/setup.py; \
+			$(SED) "s|, *'$$path'||g" $(@D)/setup.py; \
+			$(SED) "s|\"$$path\", *||g" $(@D)/setup.py; \
+			$(SED) "s|, *\"$$path\"||g" $(@D)/setup.py; \
+		done; \
+		for kind in include lib; do \
+			$(SED) "s|os.path.expanduser('~/$$kind'), *||g" $(@D)/setup.py; \
+			$(SED) "s|, *os.path.expanduser('~/$$kind')||g" $(@D)/setup.py; \
+			$(SED) "s|os.path.expanduser(\"~/$$kind\"), *||g" $(@D)/setup.py; \
+			$(SED) "s|, *os.path.expanduser(\"~/$$kind\")||g" $(@D)/setup.py; \
+		done; \
+	fi
+endef
+PYTHON_BACKPORTS_LZMA_POST_PATCH_HOOKS += PYTHON_BACKPORTS_LZMA_SANITIZE_SETUP
+PYTHON_BACKPORTS_LZMA_PRE_BUILD_HOOKS += PYTHON_BACKPORTS_LZMA_SANITIZE_SETUP
+
 define PYTHON_BACKPORTS_LZMA_EXTRACT_CMDS
 	$(INFLATE$(suffix $(PYTHON_BACKPORTS_LZMA_SOURCE))) $(PYTHON_BACKPORTS_LZMA_DL_DIR)/$(PYTHON_BACKPORTS_LZMA_SOURCE) | \
 	$(TAR) --no-same-owner --strip-components=$(PYTHON_BACKPORTS_LZMA_STRIP_COMPONENTS) \
